08cef170e2dd5eb98c2a22e3461c08bf071b1257
Nuttayos Suvisuttikasame <nuttayossuvisuttikasame@MacBook-Pro-khxngNuttayos.local>
2023-12-31 20:12:56 +0700

    - [feat] setup react hook form

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,10 +1,13 @@
-import { useEffect } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
-import { getCabins } from "../services/apiCabins";
+import Button from "../ui/Button";
 import CabinTable from "../features/cabins/CabinTable";
+import { useState } from "react";
+import CreateCabinForm from "../features/cabins/CreateCabinForm";
 
 function Cabins() {
+  const [showForm, setShowForm] = useState(false);
+
   return (
     <>
       <Row type="horizontal">
@@ -13,6 +16,10 @@ function Cabins() {
       </Row>
       <Row>
         <CabinTable />
+        <Button onClick={() => setShowForm((show) => !show)}>
+          Add new cabin
+        </Button>
+        {showForm && <CreateCabinForm />}
       </Row>
     </>
   );
