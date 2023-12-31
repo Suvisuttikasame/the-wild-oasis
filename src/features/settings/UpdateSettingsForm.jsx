5a2c9e09253fccdefd9b65b3f7a7dfaf269cd73f
Nuttayos Suvisuttikasame <nuttayossuvisuttikasame@MacBook-Pro-khxngNuttayos.local>
2024-01-01 00:59:38 +0700

    - [feat]edit setting

--- src/features/settings/UpdateSettingsForm.jsx
+++ src/features/settings/UpdateSettingsForm.jsx
@@ -2,6 +2,7 @@ import Form from "../../ui/Form";
 import FormRow from "../../ui/FormRow";
 import Input from "../../ui/Input";
 import Spinner from "../../ui/Spinner";
+import { useEditSetting } from "./useEditSetting";
 import { useSetting } from "./useSetting";
 
 function UpdateSettingsForm() {
@@ -15,21 +16,43 @@ function UpdateSettingsForm() {
     } = {},
   } = useSetting();
 
+  const { editSetting, isEditing } = useEditSetting();
+
+  function handleEdit(e, field) {
+    const { value } = e.target;
+    if (!value) return;
+    editSetting({ [field]: value });
+  }
+
   if (isLoading) <Spinner />;
 
   return (
     <Form>
       <FormRow label="Minimum nights/booking">
-        <Input type="number" id="min-nights" defaultValue={minBookingLength} />
+        <Input
+          type="number"
+          id="min-nights"
+          defaultValue={minBookingLength}
+          disabled={isEditing}
+          onBlur={(e) => handleEdit(e, "minBookingLength")}
+        />
       </FormRow>
       <FormRow label="Maximum nights/booking">
-        <Input type="number" id="max-nights" defaultValue={maxBookingLength} />
+        <Input
+          type="number"
+          id="max-nights"
+          defaultValue={maxBookingLength}
+          disabled={isEditing}
+          onBlur={(e) => handleEdit(e, "maxBookingLength")}
+        />
       </FormRow>
       <FormRow label="Maximum guests/booking">
         <Input
           type="number"
           id="max-guests"
           defaultValue={maxGuestPerBooking}
+          disabled={isEditing}
+          onBlur={(e) => handleEdit(e, "maxGuestPerBooking")}
         />
       </FormRow>
       <FormRow label="Breakfast price">
@@ -37,6 +60,8 @@ function UpdateSettingsForm() {
           type="number"
           id="breakfast-price"
           defaultValue={breakfastPrice}
+          disabled={isEditing}
+          onBlur={(e) => handleEdit(e, "breakfastPrice")}
         />
       </FormRow>
     </Form>
