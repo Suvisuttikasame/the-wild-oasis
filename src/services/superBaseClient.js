13edc6b84c9ae3e47b6e6452fcddfcda08c58a5f
nutTaY <nuttayos@hocco.co>
2024-01-03 10:54:33 +0700

    - [fix] supabase key

--- src/services/superBaseClient.js
+++ src/services/superBaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabaseUrl = "https://pkqehgstrrykufjeipws.supabase.co";
-const supabaseKey = process.env.SUPABASE_KEY;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
