8923fd6d061a2a8b4f405961136ec5e6cc03e7af
nutTaY <nuttayos@hocco.co>
2024-01-08 19:24:43 +0700

    - [feat] check out

--- src/features/check-in-out/useCheckout.js
+++ src/features/check-in-out/useCheckout.js
@@ -1,10 +1,18 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { updateBooking } from "../../services/apiBookings";
+import toast from "react-hot-toast";
 
-export function useCheckout(id, obj) {
+export function useCheckout() {
+  const queryClient = useQueryClient();
   const { mutate, isLoading, error } = useMutation({
     mutationKey: [""],
-    mutationFn: () => updateBooking(id, obj),
+    mutationFn: (bookingId) =>
+      updateBooking(bookingId, { status: "checked-out" }),
+    onSuccess: (data) => {
+      toast.success(`Booking #${data.id} successfully checked out`);
+      queryClient.invalidateQueries({ active: true });
+    },
+    onError: () => toast.error("There was an error while checking out"),
   });
   return { mutate, isLoading, error };
 }
--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -101,7 +101,6 @@ export async function getStaysTodayActivity() {
 }
 
 export async function updateBooking(id, obj) {
-  console.log(id, obj);
   const { data, error } = await supabase
     .from("bookings")
     .update(obj)
