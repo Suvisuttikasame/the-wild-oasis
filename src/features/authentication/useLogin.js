1c1cfc25273494357d882983274b5a6ad90163af
Nuttayos Suvisuttikasame <nuttayossuvisuttikasame@MacBook-Pro-khxngNuttayos.local>
2024-01-13 00:08:02 +0700

    - [feat] authentication

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -1,13 +1,17 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { login } from "../../services/apiAuth";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 
 export function useLogin() {
+  const queryClient = useQueryClient();
   const navigate = useNavigate();
   const { mutate, isLoading, error } = useMutation({
     mutationFn: ({ email, password }) => login({ email, password }),
-    onSuccess: () => navigate("/dashboard"),
+    onSuccess: (user) => {
+      queryClient.setQueriesData(["user"], user);
+      navigate("/dashboard");
+    },
     onError: () => toast.error("provided email or password is incorrect"),
   });
 
---
+++ src/ui/ProtectedRoute.jsx
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+import { useUser } from "../features/authentication/useUser";
+import Spinner from "./Spinner";
+import PropTypes from "prop-types";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+
+const FullPage = styled.div`
+  height: 100vh;
+  background-color: var(--color-grey-50);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+function ProtectedRoute({ children }) {
+  const navigate = useNavigate();
+  const { isAuthenticated, isLoading } = useUser();
+  useEffect(
+    function () {
+      if (!isAuthenticated && !isLoading) navigate("/login");
+    },
+    [isAuthenticated, isLoading, navigate]
+  );
+
+  if (isLoading)
+    return (
+      <FullPage>
+        <Spinner />
+      </FullPage>
+    );
+
+  if (isAuthenticated) return children;
+}
+
+ProtectedRoute.propTypes = {
+  children: PropTypes.node,
+};
+
+export default ProtectedRoute;
