5d3ea956374079f1fe51218a7ce2f0966247c152
Nuttayos Suvisuttikasame <nuttayossuvisuttikasame@MacBook-Pro-khxngNuttayos.local>
2024-01-14 21:43:40 +0700

    - [feat] darkmode

---
+++ src/context/DarkModeContext.jsx
@@ -0,0 +1,43 @@
+import { createContext, useContext, useEffect } from "react";
+import { useLocalStorageState } from "../hooks/useLocalStorageState";
+import PropTypes from "prop-types";
+
+const DarkModeContext = createContext();
+
+function DarkModeProvider({ children }) {
+  const [isDarkMode, setIsDarkMode] = useLocalStorageState(false, "isDarkMode");
+  function toggleDarkMode() {
+    setIsDarkMode((isDark) => !isDark);
+  }
+  useEffect(
+    function () {
+      if (isDarkMode) {
+        document.documentElement.classList.add("dark-mode");
+        document.documentElement.classList.remove("light-mode");
+      } else {
+        document.documentElement.classList.add("light-mode");
+        document.documentElement.classList.remove("dark-mode");
+      }
+    },
+    [isDarkMode]
+  );
+
+  return (
+    <DarkModeContext.Provider value={{ isDarkMode, toggleDarkMode }}>
+      {children}
+    </DarkModeContext.Provider>
+  );
+}
+
+function useDarkMode() {
+  const context = useContext(DarkModeContext);
+  if (!context)
+    throw new Error("This context was used outside DarkModeProvider");
+  return context;
+}
+
+DarkModeProvider.propTypes = {
+  children: PropTypes.node,
+};
+
+export { DarkModeProvider, useDarkMode };
